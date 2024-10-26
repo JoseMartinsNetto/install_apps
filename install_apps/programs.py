e49adb98f9e93d5ab5905ccfb5e670458b475e4a
José Martins <j.msantos.netto@gmail.com>
2024-10-26 20:33:39 -0300

    fixes

---
+++ install_apps/programs.py
@@ -0,0 +1,15 @@
+from install_apps.utils import *
+
+def install_java():
+    run_command('curl -s "https://get.sdkman.io" | bash')
+    reload_source()
+    run_command("sdk install java 17.0.12-amzn")
+
+def install_deno():
+    run_command("curl -fsSL https://deno.land/install.sh | sh")
+
+def install_rust():
+    run_command("curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh")
+
+def configure_nvim():
+    run_command("git clone https://github.com/JoseMartinsNetto/nvim2.git ~/.config/nvim")
